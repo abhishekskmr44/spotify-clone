ac1743a7ff6ce049e08cb9372f893b7e7b609fdb
Abhishek Sukumar <abhishekskmr44@gmail.com>
2022-10-04 09:36:14 +0530

    ok

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -10,7 +10,8 @@ import 'swiper/css'
 import 'swiper/css/free-mode';
 
 
-const TopChartCard = ({song,i}) => (
+const TopChartCard = ({song,i , isPlaying, activeSong,
+handlePauseClick, handlePlayClick}) => (
   <div className="w-full flex-row items-center hover:bg-
   [#4c426e] py-2 p-4 rounded-lg cursor-pointer
   mb-2">
@@ -24,8 +25,17 @@ alt={"song?.title"}/>
 <Link to={`/songs/${song.key}`}>
 <p className="text-xl font-bold text-white">{song?.title}</p>
 </Link>
+<Link to={`/artists/${song?.artists[0].adamid}`}>
+<p className="text-base text-gray-300 mt-1">{song?.subtitle}</p>
+</Link>
 </div>
  </div>
+ <PlayPause
+ isPlaying={isPlaying}
+ activeSong={activeSong}
+ song={song}
+handlePause={handlePauseClick}
+ />
   </div>
 )
 
@@ -50,7 +60,7 @@ const handlePauseClick = () => {
   dispatch(playPause(false));
 }
 
-const handlePlayClick = () => {
+const handlePlayClick = (song, i) => {
 dispatch(setActiveSong({song, data, i}))
 dispatch(playPause(true));
 }
@@ -76,6 +86,10 @@ return (
         key={song.key}
         song = {song}
            i = {i}
+           isplaying={isPlaying}
+           activeSong={activeSong}
+           handlePauseClick={handlePauseClick}
+           handlePlayClick={()=>handlePlayClick(song,i)}
         />
            
       ))}
--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,31 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux'
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components'
+import { setActiveSong, playPause } from "../redux/features/playerSlice";
+
+
+
+const SongDetails = () => {
+     const dispatch = useDispatch()
+    const {songid} = useParams()
+     const {setActiveSong, isPlaying } = useSelector((state)=>state.player)
+    
+    
+     console.log(songid)
+
+    return(
+        <div className="flex flex-col">
+       {/* <DetailsHeader artistId={artistId} songData={songData}/> */}
+        <div className="mb-10">
+            <h2 className="text-white text-3xl font-bold">
+                Lyrics:</h2>
+        </div>
+        
+        </div>
+
+    )
+
+
+}
 
 export default SongDetails;
